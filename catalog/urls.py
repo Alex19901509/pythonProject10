805dee0949ed7161f95fb1f94c61cf6f50c73baa
Alex19901509 <147518422+Alex19901509@users.noreply.github.com>
2024-07-14 20:06:33 +0300

    +interface

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_list, products_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
+    #path("", home, name="home"),
     path("contacts/", contacts, name="contacts"),
+    path("", product_list, name='catalog_list'),
+    path('products/<int:pk>/', products_detail, name='catalog_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
@@ -13,3 +15,16 @@ def contacts(request):
         print(f"{name} ({phone}): {message}")
 
     return render(request, "contacts.html")
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products_list.html", context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "products_detail.html", context)
+
